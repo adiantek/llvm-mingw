dc8be3e93539cd73777eef7485051eeff8f76448
Martin Storsjö <martin@martin.st>
2021-03-08 13:46:01 +0200

    build-compiler-rt: Do a plain "ninja install" instead of manually copying files
    This still requires manually moving the dll files to the arch specific
    runtime directory.
    
    This requires building a version of llvm past
    8e11bede3a6ac11ebcc05c82fac39899feaf9534 (March 18th 2021)

--- build-compiler-rt.sh
+++ build-compiler-rt.sh
@@ -92,15 +92,6 @@ for arch in $ARCHS; do
             ;;
         esac
     fi
-    case $arch in
-    armv7)
-        libarchname=arm
-        ;;
-    i686)
-        buildarchname=i386
-        libarchname=i386
-        ;;
-    esac
 
     [ -z "$CLEAN" ] || rm -rf build-$arch$BUILD_SUFFIX
     mkdir -p build-$arch$BUILD_SUFFIX
@@ -108,7 +99,7 @@ for arch in $ARCHS; do
     cmake \
         ${CMAKE_GENERATOR+-G} "$CMAKE_GENERATOR" \
         -DCMAKE_BUILD_TYPE=Release \
-        -DCMAKE_INSTALL_PREFIX="$PREFIX/$arch-w64-mingw32" \
+        -DCMAKE_INSTALL_PREFIX="$PREFIX/lib/clang/$CLANG_VERSION" \
         -DCMAKE_C_COMPILER=$arch-w64-mingw32-clang \
         -DCMAKE_CXX_COMPILER=$arch-w64-mingw32-clang++ \
         -DCMAKE_SYSTEM_NAME=Windows \
@@ -122,18 +113,10 @@ for arch in $ARCHS; do
         -DSANITIZER_CXX_ABI=libc++ \
         $SRC_DIR
     $BUILDCMD ${CORES+-j$CORES}
-    mkdir -p "$PREFIX/lib/clang/$CLANG_VERSION/lib/windows"
+    $BUILDCMD install
     mkdir -p "$PREFIX/$arch-w64-mingw32/bin"
-    for i in lib/windows/libclang_rt.*.a; do
-        cp $i "$PREFIX/lib/clang/$CLANG_VERSION/lib/windows/$(basename $i | sed s/$buildarchname/$libarchname/)"
-    done
-    for i in lib/windows/libclang_rt.*.dll; do
-        if [ -f $i ]; then
-            cp $i "$PREFIX/$arch-w64-mingw32/bin"
-        fi
-    done
     if [ -n "$SANITIZERS" ]; then
-        $BUILDCMD install-compiler-rt-headers
+        mv "$PREFIX/lib/clang/$CLANG_VERSION/lib/windows/"*.dll "$PREFIX/$arch-w64-mingw32/bin"
     fi
     cd ..
 done
